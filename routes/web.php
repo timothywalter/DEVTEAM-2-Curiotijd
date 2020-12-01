190295d447c96857264ae760d4d8c7fa0b8c1424
teunrompa <47944450+teunrompa@users.noreply.github.com>
2020-12-01 09:18:32 +0100

    welcome removed

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('login', function () {
     return view('login');
 });
+
+
 
 Route::get('register', function () {
     return view('register');

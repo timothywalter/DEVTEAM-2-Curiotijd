643be85ee2e3ff94a7fd2e8541b45b1958608b2c
lucthyssen <1lucthyssen@gmail.com>
2020-12-04 10:01:37 +0100

    Dashboard layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,24 @@
 
 
 @section('content')
-    <h1>dashboard</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dolor cum placeat a quae quod at illum ad, eos recusandae, blanditiis labore obcaecati delectus consequatur similique quam reprehenderit sit veritatis.</p>
+    <div class="container">
+        <h1>dashboard</h1>
+        <h2>Welkom bij Curiotijd, $name</h2>
+        <hr>
+        <div class="row">
+            {{-- Left task bar --}}
+            <div class="col-2 border">
+                <h3>Taken:</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis velit quibusdam officiis itaque impedit sapiente nesciunt voluptatibus. Itaque excepturi, fuga nisi id autem quia, modi rem vel voluptatibus minus aperiam.</p>
+            </div>
+
+            <div class="col-5">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, laudantium necessitatibus obcaecati maxime, temporibus cupiditate aliquid ipsam quisquam, sequi tempora omnis amet natus? Necessitatibus rerum accusantium, libero consectetur obcaecati deserunt.</p>
+            </div>
+            <div class="col-5">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident porro magni iusto, numquam sunt voluptas illum maiores obcaecati incidunt. Quam culpa voluptates quas, repellendus et excepturi sint maxime vero!</p> 
+            </div>
+        </div>
+    </div>
+    
 @endsection

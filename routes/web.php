1e94ccfec65fdd236cd08709cf6230892bcd20ab
lucthyssen <1lucthyssen@gmail.com>
2020-12-11 09:21:57 +0100

    Login route

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,9 @@ Route::get('register', function () {
     return view('register');
 });
 
+Route::get('/', function () {
+    return view('login');
+});
 
 Route::get('account', function () {
     return view('account');

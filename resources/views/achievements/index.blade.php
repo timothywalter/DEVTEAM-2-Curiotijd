d8874b306d7bf94c69795773db1ce97c8b7c89c9
teunrompa <47944450+teunrompa@users.noreply.github.com>
2020-12-15 13:00:59 +0100

    wda

--- resources/views/achievements/index.blade.php
+++ resources/views/achievements/index.blade.php
@@ -1,7 +1,18 @@
-<h1>Achiement dashboard</h1>
-<ul>
-    @foreach ($achievements as $achievement)
-    <li>{{$achievement['achievement']}}</li>
-    @endforeach
-    <a href="achievement.insert">Maak een nieuwe achievement aan</a>
-</ul>
+
+<div>
+    <h1>Achiement dashboard</h1>
+</div>
+<div>
+    <h2>Alle Achievements</h2>
+</div>
+<div>
+    <ul>
+        @foreach ($achievements as $achievement)
+        <li>{{$achievement['achievement']}} - exp: {{$achievement['experience']}}</li>
+        @endforeach
+      
+    </ul>
+</div>
+<div>
+      <a href="achievement.insert">Maak een nieuwe achievement aan</a>
+</div>

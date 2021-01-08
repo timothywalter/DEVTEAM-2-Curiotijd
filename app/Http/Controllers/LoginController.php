1803ab92d6a501656e69b8104ff5e0bacfcc1926
teunrompa <47944450+teunrompa@users.noreply.github.com>
2021-01-08 11:40:17 +0100

    login change

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,17 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\DB;
-
 use App\Models\Login;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function index(){
         //haalt data uit de db op
-        $students = DB::table('student')
-        ->get();
+        $students = DB::table('student')->get();
 
         $students = Login::all();
 
@@ -22,11 +20,22 @@ class LoginController extends Controller
         ]);
     }
 
-    public function loginCheck()
+    public function authenticate(Request $request)
     {
+        $credentials = $request->only('email', 'password');
 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
     }
-  
 }
+  
+
    
 
